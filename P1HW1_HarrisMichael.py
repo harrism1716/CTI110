80c547d444dc84e6e78a7f90f078dc62b11422e6
harrism1716 <harrism1716@student.faytechcc.edu>
2025-09-05 13:53:38 -0400

    Add files via upload

---
+++ P1HW1_HarrisMichael.py
@@ -0,0 +1,21 @@
+#Michael Harris
+#September 5, 2025
+#P1HW1
+#Python code using calculations
+
+print("-----Calculating Exponenets----\n")
+
+intger_1= int(input("Enter an integer as the base value: "))
+interger_2= int(input("Enter an integer as the exponent: "))
+result=intger_1 ** interger_2
+print()
+print(intger_1, "raised to the power of", interger_2, "is", result, "!!\n")
+
+print("-----Addition and Subtraction----\n")
+
+start_int= int(input("Enter a starting integer: "))
+add_int= int(input("Enter an integer to add: "))
+sub_int= int(input("Enter an integer to subtract: "))
+result_2=start_int + add_int - sub_int
+print()
+print(start_int, "+", add_int, "-", sub_int, "is equal to", result_2)
