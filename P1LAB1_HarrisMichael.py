3b9c32103b55a06eb96f2c93122496ada8547b0e
harrism1716 <harrism1716@student.faytechcc.edu>
2025-09-05 12:07:14 -0400

    Add files via upload

---
+++ P1LAB1_HarrisMichael.py
@@ -0,0 +1,8 @@
+#Michael Harris
+#September 5, 2025
+#P1_Lab1
+#Beginning of code with names
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name,last_name,"! Welcome to CTI-110")
