d14c4a89f68276567d99b8bda03f34984304f1ff
harrism1716 <harrism1716@student.faytechcc.edu>
2025-09-13 23:16:02 -0400

    Add files via upload

---
+++ P2LAB2_HarrisMichael.py
@@ -0,0 +1,29 @@
+#Michael Harris
+#September 13, 2025
+#P2_Lab2
+#Dictionary Code
+
+vehicle_mpg = {
+    "Camaro" : 18.21,
+    "Prius" : 52.36,
+    "Model S" : 110,
+    "Silverado" : 26
+}
+
+keys = vehicle_mpg.keys()
+
+print(keys)
+print()
+
+select_vehicle = input("Enter a vehicle to see it's mpg: ")
+print()
+
+mpg = vehicle_mpg[select_vehicle]
+print(f"The {select_vehicle} gets {mpg}.")
+print()
+
+miles = float(input(f"How many miles will you drive the {select_vehicle}? "))
+print()
+
+gal_needed = miles/mpg 
+print(f"{gal_needed:.2f} gallon(s) of gas are needed to drive the {select_vehicle} {miles} miles.")
